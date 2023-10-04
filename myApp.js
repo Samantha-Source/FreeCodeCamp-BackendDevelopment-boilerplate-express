fe14d85a7b590fea1010e525f37be23fcb37e78f
Samantha Brown <113550193+Samantha-Source@users.noreply.github.com>
2023-10-04 00:28:44 -0400

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -3,12 +3,17 @@ let app = express();
 
 app.use('/public', express.static(__dirname + '/public'));
 app.get("/", get);
+app.get("/json", getJson);
 
 
 
 function get(req, res) {
     // res.send('Hello Express');
     res.sendFile(__dirname + '/views/index.html');
+}
+
+function getJson(req, res) {
+    res.json({"message": "Hello json"});
 }
 
 console.log("Hello World");
